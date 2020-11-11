8596169205c279d028a835e6d01e3496e5a35817
nhzzz <61051765+nhzzz@users.noreply.github.com>
2020-11-11 20:41:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-print('You Are Running In ' + os.name + ' ' + os.getcwd())
-print('当你看到这句话说明你成功了')
-f = open('downpack.py','w')
-f.write("print ('download from Zombie github')")
+import requests as r
+print('成功连接至zombieGitHub')
+while True:
+	menu = "输入数字进行选择\n1.原理详解\n2.远程端代码\n3.exit\n"
+	choose = input(menu)
+	if choose == '1':
+		print('原理解释：如何做到两行代码实现大量功能')
+		print('实现这个功能主要运用了requests库')
+		print('从github获取代码再通过exec运行')
+	elif choose == '2':
+		print(r.get('https://nhzzz.github.io/ista/test.py').text)
+	else:
+		print('链接断开')
+		break
+	
