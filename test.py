09808a4fc4eb80afe04b5ad55aa6693f21242ec3
nhzzz <61051765+nhzzz@users.noreply.github.com>
2020-11-11 20:06:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 print('You Are Running In ' + os.name + ' ' + os.getcwd())
-os.system('mkdir sb')
+print('当你看到这句话说明你成功了')
 f = open('downpack.py','w')
 f.write("print ('download from Zombie github')")
